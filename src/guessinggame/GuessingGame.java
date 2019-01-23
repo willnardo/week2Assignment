52325d88b9811e569100254cd543734f20ef08c7
willnardo <bensoncope@gmail.com>
2019-01-22 19:25:52 -0600

    will test commit

---
+++ src/guessinggame/GuessingGame.java
@@ -0,0 +1,20 @@
+/*
+ written by William Setterberg
+January 21, 2019
+ */
+package guessinggame;
+
+/**
+ *
+ * @author Will
+ */
+public class GuessingGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
