f97f7bf27d7ade33686e386c47865f654054c70d
willnardo <bensoncope@gmail.com>
2019-01-23 18:59:57 -0600

    added login screen - need to use localhost DB

--- src/guessinggame/GuessingGame.java
+++ src/guessinggame/GuessingGame.java
@@ -4,6 +4,8 @@ January 21, 2019
  */
 package guessinggame;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Will
@@ -15,6 +17,19 @@ public class GuessingGame {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner s = new Scanner(System.in);
+        int randomNumber = (int) (Math.random() * 20 + 1);
+
+        System.out.print("I'm thinking of a number between 0-20.  What is the number? ");
+        int userGuess = s.nextInt();
+
+        if (userGuess == randomNumber) {
+            System.out.println("The number " + userGuess + " is correct!");
+        } else {
+            System.out.println("The number " + userGuess + " is not correct."
+                    + "\nThe correct number is " + randomNumber + ".");
+        }
+
     }
-    
+
 }
